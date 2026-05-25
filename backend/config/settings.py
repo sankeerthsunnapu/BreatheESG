ff84c1c43adb6070d65089bbf90ed610ceb8fd96
Sankeerth <sankeerthsunnapu@gmail.com>
2026-05-25 15:09:02 +0530

    FIx static root for Render

--- backend/config/settings.py
+++ backend/config/settings.py
@@ -10,6 +10,10 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/6.0/ref/settings/
 """
 
+import os
+import dj_database_url
+from pathlib import Path
+
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
